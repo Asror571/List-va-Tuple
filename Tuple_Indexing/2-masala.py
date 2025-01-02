614bdd27f06bc46b18678dded5e68b184b4d3041
Asror571 <asrorqurbonazarov@gmail.com>
2025-01-02 16:14:15 +0500

    Tuple_Indexing/

---
+++ Tuple_Indexing/2-masala.py
@@ -0,0 +1,2 @@
+my_tuple = ("olma", "10-sinf", "qizil", "dekabr", "45-maktab", "qurbonazarov", "asror")     # my_tuple nomli tuple yaratildi
+print(my_tuple[-1])         # Oxirgi element index orqali chiqarildi
---
+++ Tuple_Indexing/3-masala.py
@@ -0,0 +1,4 @@
+my_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 3, 9, 10, 3, 11, 12, 3)
+print(my_tuple[1],my_tuple[7]) 
+
+print("Yig'indi :",my_tuple[1]+my_tuple[7])

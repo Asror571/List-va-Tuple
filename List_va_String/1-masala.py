f775e6c186770fe72b378718c7c2f07aa566dc8d
Asror571 <asrorqurbonazarov@gmail.com>
2025-01-02 15:45:43 +0500

    List_va_String/

---
+++ List_va_String/1-masala.py
@@ -0,0 +1,6 @@
+my_list  = 'Hello world'         # my_list nomli string yaratildi 
+
+list_a  = list(my_list)          # list_a nomli o'zgaruvchi yaratildi va unga my_list ni list ro'yxatga o'takazildi 
+
+print(list_a)             # Natija chiqarildi 
+

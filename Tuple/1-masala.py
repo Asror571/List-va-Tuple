0a184b546bc5cde82ca074bd15a9b64c3385b330
Asror571 <asrorqurbonazarov@gmail.com>
2025-01-02 15:56:04 +0500

    Tuple/

---
+++ Tuple/1-masala.py
@@ -0,0 +1,2 @@
+my_tuple = (12,13,45,67,43,34,22,223,34,67)    #my_tuple  nomli tuple yaratildi va   natija chiqarildi 
+print(my_tuple)
---
+++ Tuple/2-masala.py
@@ -0,0 +1,6 @@
+my_tuple = (12,'asror','qurbonazarov','45-maktab',)     # my_tuple  nomli tuple yaratildi 
+my_tuple[1] = "Najot ta'lim"                            # va uni   1 indexdagi elementiga o'zgartirish kiritishga urinildi 
+
+
+# Natija
+#TypeError: 'tuple' object does not support item assignment
